3f68384b815429d9292940fae8bb0c7d881ba783
Nadine <nadine@soar.earth>
2021-11-01 15:05:18 +0800

    Installed styled-components and modified Button.js

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-import './Button.css';
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #ffcdb2;
+  border-radius: 20px;
+  color: #ffcdb2;
+  background: #b5838d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+  transition-duration: 0.7ms;
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ffcdb2;
+    border-color: #ffcdb2;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+    color: #b5838d;
+  }
+`;
+
+//const Button = props => {
+//  return (
+//    <button type={props.type} className="button" onClick={props.onClick}>
+//      {props.children}
+//    </button>
+//  );
+// };
 
 export default Button;

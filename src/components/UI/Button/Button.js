951d04c5542c12be0e6e6ceb084a4aefcea4901e
Nadine <nadine@soar.earth>
2021-11-02 15:36:56 +0800

    Commented out styled components codes and re-added import from React

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,40 +1,41 @@
-import styled from "styled-components";
+import React from "react";
+// import styled from "styled-components";
 
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #ffcdb2;
-  border-radius: 20px;
-  color: #ffcdb2;
-  background: #b5838d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-  transition-duration: 0.7ms;
+// const Button = styled.button`
+//  width: 100%;
+//  font: inherit;
+//  padding: 0.5rem 1.5rem;
+//  border: 1px solid #ffcdb2;
+//  border-radius: 20px;
+//  color: #ffcdb2;
+//  background: #b5838d;
+//  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//  cursor: pointer;
+//  transition-duration: 0.7ms;
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
+//  @media (min-width: 768px) {
+//   width: auto;
+//  }
 
-  &:focus {
-    outline: none;
-  }
+//  &:focus {
+//    outline: none;
+//  }
 
-  &:hover,
-  &:active {
-    background: #ffcdb2;
-    border-color: #ffcdb2;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-    color: #b5838d;
-  }
-`;
+//  &:hover,
+//  &:active {
+//    background: #ffcdb2;
+//    border-color: #ffcdb2;
+//    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//    color: #b5838d;
+//  }
+//`;
 
-//const Button = props => {
-//  return (
-//    <button type={props.type} className="button" onClick={props.onClick}>
-//      {props.children}
-//    </button>
-//  );
-// };
+const Button = (props) => {
+  return (
+    <button type={props.type} className="button" onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;

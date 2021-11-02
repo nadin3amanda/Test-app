d4a81b97718f158fbf42b10dd8cf552e4927fb74
Nadine <nadine@soar.earth>
2021-11-02 14:36:53 +0800

    Added styled components and new function FormControl in CourseInput.js

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,43 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+const FormControl =styled.div`
+margin: 0.5rem 0;
+
+& label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+& input {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+  line-height: 1.5rem;
+  padding: 0 0.25rem;
+}
+
+& input:focus {
+  outline: none;
+  background: #ffe8d6;
+  border-color: #b5838d;
+}
+
+&.invalid input {
+  border-color: red;
+  background: rgb(255, 168, 168);
+}
+
+&.invalid label {
+  color: red;
+}
+
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");

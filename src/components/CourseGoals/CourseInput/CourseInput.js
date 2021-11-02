97348657f8b83ed4d1b6b024ea6faa96237b8c61
Nadine <nadine@soar.earth>
2021-11-02 15:01:47 +0800

    Added props.imvalid in styled components for label in form control in CourseInput.js

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -11,13 +11,15 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid ${props => (props.invalid ? 'red' : #ccc)};
-    background: ${props => (props.invalid ? 'rgb(255, 168, 168)' : 'transparent')};
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) =>
+      props.invalid ? "rgb(255, 168, 168)" : "transparent"};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -56,7 +58,7 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid }>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
